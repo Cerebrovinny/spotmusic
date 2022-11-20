42189f1381c61858c36b6cf22e4e5b34946ba044
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-11-20 11:13:00 -0300

    added validation for name field

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -91,15 +91,17 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field
                 type="text"
+                name="name"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -161,7 +163,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
@@ -177,6 +179,15 @@ export default {
   data() {
     return {
       tab: "login",
+      schema: {
+        name: "required",
+        email: "",
+        age: "",
+        password: "",
+        confirm_password: "",
+        country: "",
+        tos: "",
+      },
     };
   },
   computed: {
