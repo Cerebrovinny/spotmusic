0e0ab6cb62b16386e1a332500859db0dafa156c4
Cerebrovinny <Cerebrovinny>
2022-11-24 06:21:33 -0300

    added validation for password field

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -117,29 +117,37 @@
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input
+              <vee-field
+                name="age"
                 type="number"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               />
+              <ErrorMessage class="text-red-600" name="age" />
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
+                name="password"
                 type="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
             </div>
+            <ErrorMessage class="text-red-600" name="password" />
+
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input
+              <vee-field
+                name="confirm_password"
                 type="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password"
               />
             </div>
+            <ErrorMessage class="text-red-600" name="confirm_password" />
+
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
@@ -184,9 +192,9 @@ export default {
       schema: {
         name: "required|min:3|max:100|alpha_spaces",
         email: "required|min:3|max:100|email",
-        age: "",
-        password: "",
-        confirm_password: "",
+        age: "required|min_value:18|max_value:100",
+        password: "required|min:3|max:100",
+        confirm_password: "confirmed:@password",
         country: "",
         tos: "",
       },
